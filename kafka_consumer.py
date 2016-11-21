56208cee86f795d3eee4408171615e88acff2e46
Stoyanov <ivo.stoyanov@verizon.com>
2016-11-21 09:54:53 -0800

    send/receive ok

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -3,10 +3,17 @@
 from kafka import KafkaConsumer
 from kafka import TopicPartition
 
+consumer = KafkaConsumer('mytopic')
+
+for msg in consumer:
+	print (msg)
+
+
+'''
 consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
-consumer.assign([TopicPartition('mytopic', 1)])
+consumer.assign([TopicPartition('mytopic', 2)])
 
 print("waiting...")
 msg = next(consumer)
 print(msg)
-
+'''
--- kafka_producer.py
+++ kafka_producer.py
@@ -1,8 +1,23 @@
+from kafka.client import KafkaClient
+from kafka.producer import SimpleProducer
+from kafka.producer import KafkaProducer
+from time import sleep
+from datetime import datetime
 import json
-from kafka import KafkaProducer
 
 print("kafka sender")
 
+#producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+for _ in range(5):
+	print("sending...")
+	#producer.send('mytopic', {'foo': 'bar'})
+	producer.send('mytopic', b'MESSAGE')
+	producer.flush()
+	sleep(1)
+
+'''
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 for _ in range(3):
@@ -11,3 +26,14 @@ for _ in range(3):
 
 # Block until all pending messages are sent
 producer.flush()
+
+#kafka = KafkaClient("localhost:9092")
+kafka = KafkaClient()
+
+producer = SimpleProducer(kafka)
+
+while 1:
+  producer.send_messages("mytopic", "Message " + str(datetime.now().time()) )
+  sleep(1)
+
+'''
