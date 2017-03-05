4ee54035c0d0cea6fc1c12644280165bc8852bae
stoyaiv <ivo.stoyanov@verizon.com>
2017-03-05 11:03:03 -0800

    wip

--- tsp_anealing/TravelingSalesmanProblem.py
+++ tsp_anealing/TravelingSalesmanProblem.py
@@ -3,6 +3,7 @@ import copy
 #import json
 # import numpy as np  # contains helpful math functions like numpy.exp()
 # import numpy.random as random  # see numpy.random module
+
 import math
 import random  # alternative to numpy.random module
 #
