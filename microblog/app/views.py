244e02e1b90422f00e2a28d70f01ec2e625cbc9a
stoyaiv <ivo.stoyanov@verizon.com>
2017-03-11 18:48:56 -0800

    forms

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,23 +1,35 @@
-from flask import render_template
+import requests
+from flask import render_template, flash, redirect
+from .forms import LoginForm
 from app import app
+
+url = 'https://jsonplaceholder.typicode.com'
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    print('INDEX')
     user = {'nickname': 'Ivo'}
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html',
-                           title='Home',
-                           user=user,
-                           posts=posts)
+    r = requests.get(url + '/posts')
+    print('status', r.status_code)
+    if r.status_code == 200:
+        return render_template('index.html',
+                                     title='Home',
+                                     user=user,
+                                     posts=r.json())
 
+
+@app.route('/configure', methods=['GET', 'POST'])
+def config():
+    print('CONFIG')
+    form = LoginForm()
+    if form.validate_on_submit():
+        print("Name", form.name.data)
+        flash('Login requested for name="%s", remember_me=%s' %
+              (form.name.data, str(form.remember_me.data)))
+
+        return redirect('/index')
+    return render_template('config.html',
+                                 title='Configuration',
+                                 form=form)
