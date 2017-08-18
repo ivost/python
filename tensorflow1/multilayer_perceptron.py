64b9b107b924c6b11aa47acce8dd098938906185
ivost <ivostoy@yahoo.com>
2017-08-18 08:55:31 -0700

    tf

--- tensorflow1/multilayer_perceptron.py
+++ tensorflow1/multilayer_perceptron.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 '''
 A Multilayer Perceptron implementation example using TensorFlow library.
 This example is using the MNIST database of handwritten digits
@@ -13,66 +7,49 @@ Author: Aymeric Damien
 Project: https://github.com/aymericdamien/TensorFlow-Examples/
 '''
 
-
-# In[2]:
-
-
 import tensorflow as tf
 import input_data
 
-
-# In[3]:
-
-
 # Import MINST data
-#from tensorflow.examples.tutorials.mnist import input_data
+# from tensorflow.examples.tutorials.mnist import input_data
+
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
-
-# In[4]:
 
 
 # Parameters
 learning_rate = 0.001
-training_epochs = 1 # 15
+training_epochs = 1  # 15
 batch_size = 100
-display_step = 1
+display_step = 10
 
 # Network Parameters
-n_hidden_1 = 256 # 1st layer number of features
-n_hidden_2 = 256 # 2nd layer number of features
-n_input = 784 # MNIST data input (img shape: 28*28)
-n_classes = 10 # MNIST total classes (0-9 digits)
+n_hidden_1 = 256  # 1st layer number of features
+n_hidden_2 = 256  # 2nd layer number of features
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
 
 # tf Graph input
 x = tf.placeholder("float", [None, n_input])
 y = tf.placeholder("float", [None, n_classes])
 
 
-# In[5]:
-
-
 # Create model
 def multilayer_perceptron(x, weights, biases):
-    
     # Hidden layer with RELU activation
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
     layer_1 = tf.nn.relu(layer_1)
     print('layer_1', layer_1.shape)
-    
+
     # Hidden layer with RELU activation
     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     layer_2 = tf.nn.relu(layer_2)
     print('layer_2', layer_2.shape)
-    
+
     # Output layer with linear activation
     out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
     print('out_layer shape', out_layer.shape)
-    
+
     return out_layer
-
-
-# In[9]:
 
 
 # Store layers weight & bias
@@ -91,15 +68,14 @@ biases = {
 pred = multilayer_perceptron(x, weights, biases)
 
 # Define loss and optimizer
+print('pred', pred)
+print('Y', y)
+
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # Initializing the variables
 init = tf.global_variables_initializer()
-
-
-# In[10]:
-
 
 # Launch the graph
 with tf.Session() as sess:
@@ -108,36 +84,28 @@ with tf.Session() as sess:
     # Training cycle
     for epoch in range(training_epochs):
         avg_cost = 0.
-        total_batch = int(mnist.train.num_examples/batch_size)
+        total_batch = int(mnist.train.num_examples / batch_size)
         # Loop over all batches
         for i in range(total_batch):
             batch_x, batch_y = mnist.train.next_batch(batch_size)
+            # if epoch % display_step == 0:
+            #     print('batch_x', batch_x.shape)
+            #     print('batch_y', batch_y.shape)
+            # batch_x(100, 784)
+            # batch_y(100, 10)
             # Run optimization op (backprop) and cost op (to get loss value)
             _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
                                                           y: batch_y})
-            print('cost', c)
             # Compute average loss
             avg_cost += c / total_batch
         # Display logs per epoch step
         if epoch % display_step == 0:
-            print ("Epoch:", '%04d' % (epoch+1), "cost=",                 "{:.9f}".format(avg_cost))
+            print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(avg_cost))
+
     print("Optimization Finished!")
 
     # Test model
     correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    print ("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+    print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
