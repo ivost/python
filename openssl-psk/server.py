389348e9ecad90657886babaed06e90ee65f3138
ivo <ivostoy@gmail.com>
2018-01-09 15:05:11 -0800

    crashes with py 2.7 and 3.5

--- openssl-psk/server.py
+++ openssl-psk/server.py
@@ -4,20 +4,27 @@ import socket
 import ssl
 import sslpsk
 
-PSKS = {'client1' : 'abcdef',
-        'client2' : '123456'}
+# preshared keys
+
+PSKS = {'client1': 'abcdef',
+        'client2': '123456'}
+
 
 def server(host, port):
     tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_sock.bind((host, port))
+
+    print('ssl-psk server listening on {}:{}'.format(host, port))
+
     tcp_sock.listen(1)
 
     sock, _ = tcp_sock.accept()
+
     ssl_sock = sslpsk.wrap_socket(sock,
-                                  server_side = True,
-				  ssl_version=ssl.PROTOCOL_TLSv1,
+                                  server_side=True,
+                                  ssl_version=ssl.PROTOCOL_TLSv1,
                                   psk=lambda identity: PSKS['identity'],
-				  hint='server1')
+                                  hint='server1')
 
     msg = ssl_sock.recv(4)
     print('Server received: ' + msg)
@@ -26,11 +33,12 @@ def server(host, port):
     ssl_sock.shutdown(socket.SHUT_RDWR)
     ssl_sock.close()
 
+
 def main():
-    print("ssl-psk server")
     host = '127.0.0.1'
     port = 6000
+    server(host, port)
 
-def __name__ == '__main__':
+
+if __name__ == '__main__':
     main()
-
