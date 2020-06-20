cd522f5d5cffc7eea43c4e74840351c280a7430a
Ivo Stoyanov <ivostoy@gmail.com>
2020-06-20 14:51:27 -0700

    wip

--- iot/app.py
+++ iot/app.py
@@ -17,6 +17,34 @@ file_name = "techcrunch.csv"
 import time
 import datetime
 
+import struct
+import paho.mqtt.client as mqtt
+import time
+from random import seed, randint
+
+# MQTT
+
+def on_receive(client, userdata, message):
+    topic = str(message.topic)
+    # message = str(message.payload.decode("utf-8"))
+    val = struct.unpack("h", message.payload)[0]
+    print(topic + ": " + str(val/100.))
+
+def main_mqtt():
+    thingy = mqtt.Client("thingy52")
+    thingy.connect("test.mosquitto.org", 1883)
+    thingy.subscribe("temperature")
+    thingy.on_message = on_receive
+    thingy.loop_start()
+    seed(1)
+
+    # Main program loop
+    while 1:
+        value = randint(2400, 2600)
+        v = struct.pack("h", value)
+        thingy.publish("temperature", v)  # Publish message to MQTT broker
+        time.sleep(1)  # Sleep for a second
+
 
 def lines(file_path):
     for line in open(file_path):
