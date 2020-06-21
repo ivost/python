415b819ecae302edc59b5ca5b423a3179d1c1557
Ivo Stoyanov <ivostoy@gmail.com>
2020-06-20 19:15:35 -0700

    wip

--- iot/pub.py
+++ iot/pub.py
@@ -6,11 +6,12 @@ topic = "ivo/temp"
 
 client = mqtt.Client("mypub")
 
-print("connecting to broker")
-client.connect(broker, 1883) 
+print("connecting to broker", broker)
+client.connect(broker, 1883)
 
-for i in range(10):
-	time.sleep(4) # wait
-	print("Publishing message to topic", topic)
-	client.publish(topic, str(i))
-
+for i in range(12):
+    time.sleep(1)
+    now = int(time.time())
+    msg = f"{now},tc,{i}"
+    print(f"sending {topic}:{msg}")
+    client.publish(topic, msg)
--- iot/sub.py
+++ iot/sub.py
@@ -2,24 +2,24 @@ import paho.mqtt.client as mqtt
 import time
 
 def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+    msg = str(message.payload.decode("utf-8"))
+    print(f"got {message.topic}:{msg}")
 
 broker="broker.emqx.io"
 topic = "ivo/temp"
 
-client = mqtt.Client("P2") #create new instance
+client = mqtt.Client("mysub") #create new instance
 
-print("connecting to broker")
-client.connect(broker, 1883) 
+print("connecting to broker " + broker)
+client.connect(broker, 1883)
+
+print("subscribing to topic " + topic)
 client.subscribe(topic)
 
 client.on_message=on_message
 
 client.loop_start()    #start the loop
-
-time.sleep(100) # wait
+while True:
+    time.sleep(1) # wait
 
 client.loop_stop() #stop the loop
