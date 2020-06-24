5cef1e02a1aa3bc03a6cbdea0b87f458566de321
ivo <ivostoy@gmail.com>
2020-06-24 09:28:34 -0700

    wip

--- iot/pub.py
+++ iot/pub.py
@@ -8,13 +8,15 @@ topic = "ivo/tc"
 #topic = "sc160_temp"
 
 client = mqtt.Client("mypub")
-
+val = 2501
+id = 1122334455
 print("connecting to broker", broker, "topic", topic)
 client.connect(broker, 1883)
 
-for i in range(12000):
+for i in range(100):
     time.sleep(10)
     now = int(time.time())
-    msg = f"{now},123456,{i}"
-    print(f"sending {topic}:{msg}")
+    msg = "{},{},{}".format(now, id, val)
+    print("sending ", topic, msg)
     client.publish(topic, msg)
+    val = val + 20
