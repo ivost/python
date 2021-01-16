3a9d9733f1f646c783953ad2552553092148589a
Ivo Stoyanov <ivostoy@gmail.com>
2021-01-15 18:35:35 -0800

    wip

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,14 +1,21 @@
 """
 
 """
+import os
+from os.path import isfile, join
 import requests
 from bs4 import BeautifulSoup
 
+MAX=100
 KEYWORD="birds"
+OUTDIR=join(os.getenv("HOME"), "Pictures")
+
 url = f"https://www.google.com/search?q={KEYWORD}&tbm=isch&ved=2ahUKEwjgrPq3n8vqAhWViZ4KHYxoC9cQ2-cCegQIABAA&oq=people+face+closeup&gs_lcp=CgNpbWcQA1DRNljRNmCYOWgAcAB4AIABT4gBT5IBATGYAQCgAQGqAQtnd3Mtd2l6LWltZw&sclient=img&ei=8NsMX-D0DpWT-gSM0a24DQ&start=##&sa=N"
 
 idx = 1
 fcount = 0
+out_dir = join(OUTDIR, KEYWORD)
+os.makedirs(out_dir, exist_ok=True)
 
 done = False
 while not done:
@@ -40,12 +47,15 @@ while not done:
         if response.status_code != 200:
             continue
 
-        file = f'/tmp/{url2[pos1+1:pos2]}.jpg'
+        file = f'{url2[pos1+1:pos2]}.jpg'
+        file = join(out_dir, file)
         print("writing ", fcount, file)
         with open(file, 'wb') as f:
             f.write(response.content)
             f.close()
             fcount += 1
+            if fcount > MAX:
+                done = True
 
 print("files:", fcount)
 
