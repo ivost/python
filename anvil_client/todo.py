4fd28bf3586cc1592c9f8eaf062fdc0678116119
Ivo Stoyanov <ivostoy@gmail.com>
2021-04-18 15:15:53 -0700

    works with remote tasks

--- anvil_client/todo.py
+++ anvil_client/todo.py
@@ -1,19 +1,30 @@
 # https://pymotw.com/2/pprint/
-from pprint import pprint
 
 import anvil.server
 from anvil.tables import app_tables
 
+"""
+https://anvil.works/docs/client/components/basic#fileloader
+
+"""
 
 class Todo:
 
     def __init__(self):
         # anvil.server.connect("[uplink-key goes here]", url="ws://your-runtime-server:3030/_/uplink")
-        key = "abcd"
-        url = "ws://mk8.westus2.cloudapp.azure.com:3030/_/uplink"
+        key = "@1@1@"
+        host = "mk8.westus2.cloudapp.azure.com"
+        port = 3030
+        url = f"ws://{host}:{port}/_/uplink"
         print("key", key)
         print("url", url)
         anvil.server.connect(key, url=url)
+        # only on server side
+        # https://anvil.works/docs/data-tables/indexing-and-sql
+        # cs = anvil.tables.get_connection_string(
+        #     via_host=host,
+        #     via_port=port)
+        # print(cs)
 
     def get_tasks(self):
         return anvil.server.call('get_tasks')
@@ -27,8 +38,10 @@ class Todo:
 
 todo = Todo()
 
-# todo.add_task({"name": "foo", "completed": True})
+# todo.add_task({"name": "foo", "complete": True})
 todo.add_task("foo")
+
+app_tables.tasks.add_row(name="bar1", complete=False)
 
 task = app_tables.tasks.get(name="foo")
 if task:
@@ -38,7 +51,6 @@ if task:
 for task in app_tables.tasks.search():
     print(task.get_id(), task['name'], task['complete'])
     # todo.delete_task(task)
-
 
 '''
 https://anvil.works/docs/data-tables/data-tables-in-code#searching-querying-a-table
